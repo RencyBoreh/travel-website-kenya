3ceaed77722b44f6a6808ad239f3c063694b9eb6
RencyBoreh <jeptanuirency313@gmail.com>
2025-02-03 20:01:42 +0300

    js

--- javascript/script.js
+++ javascript/script.js
@@ -2,3 +2,40 @@ console.log("Hello");
 console.log("First js practice");
 // Variables
 // store information-containers
+console.log("welcome to variables");
+var names='Rency Jeptanui Boreh';
+console.log(names);
+var name="Rency";
+console.log(name);
+const myname="rency boreh";
+console.log(myname)
+ var name="boreh";
+ console.log(name);
+
+
+//  Data Types
+// string
+let name1="Resly Jerop"; //can be single or double quotes
+console.log(name1);
+// numbers
+let x=10;
+let y=12.5;
+console.log(x+y);
+// booleans
+let student=true;
+let drinking=false;
+console.log(student);
+// null
+let name2=null;
+console.log(name2);
+// undefined
+let name3;
+console.log(name3);
+// object
+let students={
+  name:"Rency Boreh",
+  age:10,
+  course:"Software Engineering"
+}
+console.log(students)
+console.log(students.name + "," + students.age + ',' + students.course);

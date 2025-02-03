5e2c21edf81b301cd5da60cc6338309f47337e3e
RencyBoreh <jeptanuirency313@gmail.com>
2025-02-03 20:26:12 +0300

    operators

--- javascript/script.js
+++ javascript/script.js
@@ -26,8 +26,10 @@ let student=true;
 let drinking=false;
 console.log(student);
 // null
-let name2=null;
+let name2=null; // empty values that will be changed later
 console.log(name2);
+name2="Boreeh";
+console.log(name2)
 // undefined
 let name3;
 console.log(name3);
@@ -39,3 +41,11 @@ let students={
 }
 console.log(students)
 console.log(students.name + "," + students.age + ',' + students.course);
+// backticks
+let first_name="Resly";
+let last_name="Jerop";
+let age=22;
+console.log(`Hello ${first_name} , You are ${age} years old`);
+// arrays
+const arr=[1,2,3,4,5,6,7];
+console.log(arr);

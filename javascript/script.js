458423fece6cd79ce078502cf06c52e9e810adde
RencyBoreh <jeptanuirency313@gmail.com>
2025-02-18 21:44:30 +0300

    classes

--- javascript/script.js
+++ javascript/script.js
@@ -415,3 +415,26 @@ console.log(n.next()) // Returns 10
 console.log(n.next()) // Returns 20
 console.log(n.next()) // Returns 30
 console.log(n.next())
+
+
+class Car {
+  constructor(brand) {
+    this.carname = brand;
+  }
+  present() {
+    return 'I have a ' + this.carname;
+  }
+}
+
+class Model extends Car {
+  constructor(brand, mod) {
+    super(brand);
+    this.model = mod;
+  }
+  show() {
+    return this.present() + ', it is a ' + this.model;
+  }
+}
+
+let myCar = new Model("Ford", "Mustang");
+document.getElementById("demo").innerHTML = myCar.show();

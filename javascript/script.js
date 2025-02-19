742d640a37e8c10786c9e9cbbfe89f8767f7f2ae
RencyBoreh <jeptanuirency313@gmail.com>
2025-02-19 10:25:25 +0300

    animation

--- javascript/script.js
+++ javascript/script.js
@@ -454,3 +454,20 @@ class Car {
 const myCar = new Car("Ford");
 
 document.getElementById("demo").innerHTML = myCar.cnam;
+
+function myMove() {
+  let id = null;
+  const elem = document.getElementById("animate");   
+  let pos = 0;
+  clearInterval(id);
+  id = setInterval(frame, 2);
+  function frame() {
+    if (pos == 350) {
+      clearInterval(id);
+    } else {
+      pos++; 
+      elem.style.top = pos + "px"; 
+      elem.style.left = pos + "px"; 
+    }
+  }
+}

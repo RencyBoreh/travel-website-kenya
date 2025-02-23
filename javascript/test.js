c3cf23c03e9c6cb5922c6280809376b9ab3f1a79
RencyBoreh <jeptanuirency313@gmail.com>
2025-02-23 17:54:07 +0300

    changes

--- javascript/test.js
+++ javascript/test.js
@@ -6,7 +6,14 @@ document.querySelectorAll('.content').forEach(function(section){
 });
 
 
-
 //show the selected content section
 document.getElementById(contentId).style.display='block';
+};
+
+function submission(){
+  let firstname=document.getElementById('firstname').value;
+  let lastname=document.getElementById('lastname').value;
+  let fullname= firstname + " " + lastname;
+  alert(`Thank ${fullname} for reaching out.I'll get back to you as soon as possible!`);
 }
+//  $('body').scrollspy({ target: '#navbar', offset: 70 });
